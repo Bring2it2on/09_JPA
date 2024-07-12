5fdc94fee34d9a0a75956ff27e893d2be0da0898
지동현 <yjkl0516@gmail.com>
2024-07-12 11:33:34 +0900

    first commit

---
+++ chap00-lecture-source/src/main/java/com/ohgiraffers/section01/problem/Category.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section01.problem;
+
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Category {
+
+    private int CategoryCode;
+    private String CategoryName;
+}
